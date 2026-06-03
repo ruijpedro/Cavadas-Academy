7c701ad3a39f9400c0944f313dff835b74f5554a
ruijpedro <rjpedro@gmail.com>
2026-06-03 19:59:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({plugins:[react()],build:{outDir:'dist'}})
+
+export default defineConfig({
+  base: '/Cavadas-Academy/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
